e849b2a56f4285f57adfb95225ffdf7f35f65154
johan aji setiawan <johanajisetiawan1118@gmail.com>
2020-12-25 00:38:36 +0700

    Updates

---
+++ precache-manifest.8bb889b837ccf7ec4721e13522641d93.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8756714c641361933c8593dc0486bba",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/index.html"
+  },
+  {
+    "revision": "6c7ceffdfa0ec260f939",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "c9abab12447df38aed0b",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/css/main.5ab9880c.chunk.css"
+  },
+  {
+    "revision": "59f432c90c27a5c5ea04",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/js/2.420480fb.chunk.js"
+  },
+  {
+    "revision": "6c7ceffdfa0ec260f939",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/js/3.28ccf4a0.chunk.js"
+  },
+  {
+    "revision": "c9abab12447df38aed0b",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/js/main.d4394e9b.chunk.js"
+  },
+  {
+    "revision": "bfd12ac14f7a4716dba3",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/js/runtime-main.d4bbf182.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "cca33bbd4a3ff094910bd88c0fa1b679",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/amikom.cca33bbd.png"
+  },
+  {
+    "revision": "86c1bb7d2b074cde3883c73471ddbed3",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/borobudur.86c1bb7d.jpg"
+  },
+  {
+    "revision": "897991ab7fac37ee94d7b1ce59b11783",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/ccna.897991ab.jpg"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "c24ba79111a90758a3a472a906ab2901",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/googleAssistant.c24ba791.svg"
+  },
+  {
+    "revision": "b05ae5b575a7e1125610944dc152c35f",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/jsFramework.b05ae5b5.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "449eec1dd1cb7659f96b7ba237ff3e78",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/pmiapp.449eec1d.jpg"
+  },
+  {
+    "revision": "c1a95f7335cbfb730b937e19e8830a53",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/programmer.c1a95f73.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "b48e86c9a956d3a3a32ca4a5ae05ddc7",
+    "url": "%3Cjohanaji%3E.github.io/portofolio/static/media/talksCardBackAlt.b48e86c9.svg"
+  }
+]);
